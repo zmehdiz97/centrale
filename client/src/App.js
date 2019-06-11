0b3d50855c4d3651394b280f2523839a5409e054
AleBlondin <alexandreblondin@laposte.net>
2019-06-11 20:50:03 +0200

    :sparkles: API call example

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import superagent from "superagent";
 
-function App() {
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await superagent.get(
+          "http://localhost:8000/application/user/last/first"
+        );
+        console.log(res);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
